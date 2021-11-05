aaf5d92676634c456627db080fcbf783746b7dbc
Rajiv Sinclair <rajivsinclair@users.noreply.github.com>
2021-11-04 23:30:49 -0700

    test-google-doc-embed

--- frontend/i18n/en/home.js
+++ frontend/i18n/en/home.js
@@ -2,7 +2,7 @@ export default {
   mainTitle: 'LLEAD Document Tagging',
   getStarted: '<iframe src="https://docs.google.com/document/d/e/2PACX-1vQxeNvN5RM4fib7kNLWMZOhK73xHK9j0hMbOxPikcnYP_rgqjxPXPq_zLQW9MtHMn9ptgJa3Fo1pTgG/pub?embedded=true"></iframe>',
   startAnnotation: 'Start Tagging',
-  featuresTitle: 'The best features',
+  featuresTitle: '<iframe src="https://docs.google.com/document/d/e/2PACX-1vQxeNvN5RM4fib7kNLWMZOhK73xHK9j0hMbOxPikcnYP_rgqjxPXPq_zLQW9MtHMn9ptgJa3Fo1pTgG/pub?embedded=true"></iframe>',
   featuresTitle1: 'Team Collaboration',
   featuresText1: 'Annotation with your team mates',
   featuresTitle2: 'Any Language',
