fb9d36f85a041cdbb8228978b0363c28d6f78fa0
Rajiv Sinclair <rajivsinclair@users.noreply.github.com>
2022-07-19 23:10:04 -0700

    Revert "Set staticfiles=False to django_heroku"
    This reverts commit 8e9b0381c72536b65c718e0583e898d87f04aa5a.

--- backend/config/settings/heroku.py
+++ backend/config/settings/heroku.py
@@ -2,4 +2,4 @@ import django_heroku
 
 from .base import *  # noqa: F401,F403
 
-django_heroku.settings(locals(), test_runner=False, staticfiles=False)
+django_heroku.settings(locals(), test_runner=False)
